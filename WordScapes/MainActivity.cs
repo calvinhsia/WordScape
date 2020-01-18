0486fa3c4d09ffad414f49c2cee53f932f718d83
Calvin Hsia <calvinh@microsoft.com>
2020-01-18 15:35:42 -0800

    andr timer

--- WordScapes/MainActivity.cs
+++ WordScapes/MainActivity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -12,18 +14,114 @@ namespace WordScapes
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public const int idtxtTimer = 10;
+
+        public const int idBtnPlayAgain = 60;
+        public const int idBtnShuffle = 70;
+        TextView _txtTimer;
+        Button _btnNew;
+
+        void createLayout()
+        {
+            SetContentView(Resource.Layout.content_main);
+            var layout = FindViewById<RelativeLayout>(Resource.Id.container);
+            _btnNew = new Button(this)
+            {
+                Text = "Play Again"
+            };
+            _btnNew.Click += BtnNew_Click;
+            layout.AddView(_btnNew);
+
+            _txtTimer = new TextView(this)
+            {
+                Id = idtxtTimer,
+                Text = "timer",
+                TextSize = 30
+            };
+            layout.AddView(_txtTimer);
+            SetLayoutForOrientation(Android.Content.Res.Orientation.Portrait);
+        }
+
+        private void SetLayoutForOrientation(Android.Content.Res.Orientation orientation)
+        {
+            switch (orientation)
+            {
+                case Android.Content.Res.Orientation.Portrait:
+                    _txtTimer.LayoutParameters = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent);
+                    ((RelativeLayout.LayoutParams)(_txtTimer.LayoutParameters)).AddRule(LayoutRules.AlignParentRight);
+
+                    break;
+            }
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            createLayout();
+            BtnNew_Click(_btnNew, null);
+            //Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+        }
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+        bool _timerEnabled = false;
+        CancellationTokenSource _cts = null;
+        int _nSecondsElapsed = 0;
+        Task _tskTimer;
+        private async void BtnNew_Click(object sender, EventArgs e)
+        {
+            if (_cts != null)
+            {
+                _cts.Cancel();
+                _timerEnabled = false;
+                await _tskTimer;
+            }
+            _cts = new CancellationTokenSource();
+            _nSecondsElapsed = 0;
+            _tskTimer = Task.Run(async () =>
+            {
+                while (!_cts.IsCancellationRequested)
+                {
+                    RunOnUiThread(() =>
+                    {
+                        if (_timerEnabled)
+                        {
+                            _txtTimer.Text = $"{GetTimeAsString(_nSecondsElapsed)}";
+                            _nSecondsElapsed++;
+                        }
+                    });
+                    await Task.Delay(1000);
+                }
+            });
+            _txtTimer.Text = string.Empty;
+            _nSecondsElapsed = 0;
+            _timerEnabled = true;
+        }
+
+        public static string GetTimeAsString(int tmpSecs)
+        {
+            var hrs = string.Empty;
+            var mins = string.Empty;
+            string secs;
+            if (tmpSecs >= 3600)
+            {
+                hrs = $"{tmpSecs / 3600:n0}:";
+                tmpSecs -= (tmpSecs / 3600) * 3600;
+            }
+            if (!string.IsNullOrEmpty(hrs) || tmpSecs >= 60)
+            {
+                mins = $"{(tmpSecs / 60).ToString(String.IsNullOrEmpty(hrs) ? "" : "00")}:";
+                tmpSecs -= (tmpSecs / 60) * 60;
+                secs = tmpSecs.ToString("00");
+            }
+            else
+            {
+                secs = tmpSecs.ToString();
+            }
+            return $"{hrs}{mins}{secs}";
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -45,7 +143,7 @@ namespace WordScapes
 
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
+            View view = (View)sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
@@ -55,6 +153,6 @@ namespace WordScapes
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+    }
 }
 
